a76cf0fbf7eb0125ecb84659f312b392392b0334
Ahmed Montaser <43428175+amontaser@users.noreply.github.com>
2024-10-18 19:55:28 +0300

    avzone

--- terraform-eks-deployment/backend.tf
+++ terraform-eks-deployment/backend.tf
@@ -4,7 +4,7 @@ terraform {
     # Specify the name of the S3 bucket to store the state file
     bucket = "supermario2030"
     # Specify the AWS region where the bucket is located
-    region = "us-east-1a"
+    region =  ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
     # Specify the path within the bucket to store the state file
     key = "terraform.tfstate"
   }
