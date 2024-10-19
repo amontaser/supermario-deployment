0f2ba14ea014c2c8cd6f466fb052ee60dd3906aa
Ahmed Montaser <43428175+amontaser@users.noreply.github.com>
2024-10-19 14:00:17 +0300

    test

--- terraform-eks-deployment/eks.tf
+++ terraform-eks-deployment/eks.tf
@@ -1,27 +1,100 @@
-# eks.tf
+# Create an IAM Role for the EKS Cluster with necessary policies
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
 
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+}
+
+# Attach AmazonEKSClusterPolicy to the EKS Cluster role
+resource "aws_iam_role_policy_attachment" "cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# Attach AmazonEKSServicePolicy for EKS cluster control plane
+resource "aws_iam_role_policy_attachment" "cluster_service_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# Create IAM Role for worker nodes (EC2 instances)
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+# Attach necessary policies for worker nodes
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_readonly_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# Create the EKS cluster and node group with proper IAM roles
 module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 20.0"
-    cluster_name = "supermario-eks-cluster"
-    cluster_version = "1.30"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-    cluster_endpoint_public_access  = true
+  cluster_name                  = "supermario-eks-cluster"
+  cluster_version               = "1.30"
+  cluster_endpoint_public_access = true
 
-    vpc_id = module.my-vpc.vpc_id
-    subnet_ids = module.my-vpc.private_subnets
+  vpc_id     = module.my-vpc.vpc_id
+  subnet_ids = module.my-vpc.private_subnets
 
-    tags = {
-        environment = "development"
-        application = "myapp"
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+
+  # Use the IAM role created above for the EKS cluster
+  iam_role_arn = aws_iam_role.eks_cluster_role.arn
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+      instance_types = ["t2.micro"]
+      iam_role_arn   = aws_iam_role.eks_node_role.arn
     }
+  }
 
-    eks_managed_node_groups = {
-        dev = {
-            min_size = 1
-            max_size = 2
-            desired_size = 1
-            instance_types = ["t2.micro"]
-        }
-    }
+  # Ensure IAM roles and policies are created before creating the cluster and node group
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_policy_attachment,
+    aws_iam_role_policy_attachment.cluster_service_policy_attachment,
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_readonly_policy
+  ]
 }
